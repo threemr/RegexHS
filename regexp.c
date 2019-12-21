82dbe795c221c0e6ad3ec54944d13c805fb13d15
TDS <tulsa.data.science@gmail.com>
2019-12-21 02:28:43 -0800

    Merged regmagic.h with regexp.h, and regerror.c and regsub.c with regexp.c

--- regexp.c
+++ regexp.c
@@ -9,8 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "regexp.h"
-#include "regmagic.h"
+
 
 /*
  * The "internal use only" fields in regexp.h are present to pass info from
@@ -1044,3 +1045,68 @@ static char *regprop(char *op)
 	return(buf);
 }
 #endif
+
+
+
+/*
+ * Former contents from regsub.c
+ * regsub - perform substitutions after a regexp match
+ */
+void regsub(const regexp *rp, const char *source, char *dest)
+{
+	register regexp * const prog = (regexp *)rp;
+	register char *src = (char *)source;
+	register char *dst = dest;
+	register char c;
+	register int no;
+	register size_t len;
+
+	if (prog == NULL || source == NULL || dest == NULL) {
+		regerror("NULL parameter to regsub");
+		return;
+	}
+	if ((unsigned char)*(prog->program) != MAGIC) {
+		regerror("damaged regexp");
+		return;
+	}
+
+	while ((c = *src++) != '\0') {
+		if (c == '&')
+			no = 0;
+		else if (c == '\\' && isdigit(*src))
+			no = *src++ - '0';
+		else
+			no = -1;
+
+		if (no < 0) {	/* Ordinary character. */
+			if (c == '\\' && (*src == '\\' || *src == '&'))
+				c = *src++;
+			*dst++ = c;
+		} else if (prog->startp[no] != NULL && prog->endp[no] != NULL &&
+					prog->endp[no] > prog->startp[no]) {
+			len = prog->endp[no] - prog->startp[no];
+			(void) strncpy(dst, prog->startp[no], len);
+			dst += len;
+			if (*(dst-1) == '\0') {	/* strncpy hit NUL. */
+				regerror("damaged match string");
+				return;
+			}
+		}
+	}
+	*dst++ = '\0';
+}
+
+
+/*
+ * Former contents from regerror.c
+ */
+void regerror(char *s)
+{
+#ifdef ERRAVAIL
+	error("regexp: %s", s);
+#else
+	fprintf(stderr, "regexp(3): %s\n", s);
+	exit(EXIT_FAILURE);
+#endif
+	/* NOTREACHED */
+}
--- regexp.h
+++ regexp.h
@@ -19,3 +19,11 @@ extern regexp *regcomp(const char *re);
 extern int regexec(regexp *rp, const char *s);
 extern void regsub(const regexp *rp, const char *src, char *dst);
 extern void regerror(char *message);
+
+/*
+ * Former contents from regmagic.h
+ *
+ * The first byte of the regexp internal "program" is actually this magic
+ * number; the start node begins in the second byte.
+ */
+#define	MAGIC	0234
